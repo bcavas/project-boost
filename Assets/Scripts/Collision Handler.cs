f6f4e389597ee505ffa8d8b3684ab785c5d3c14c
bcavas <35956598+bcavas@users.noreply.github.com>
2024-09-29 16:36:12 +0800

    implement level progression

--- Assets/Scripts/Collision Handler.cs
+++ Assets/Scripts/Collision Handler.cs
@@ -4,9 +4,11 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     int currentSceneIndex;
+    int totalSceneCount;
     void Start() // get scene index upon initialization of parent object (the rocket ship)
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        totalSceneCount = SceneManager.sceneCountInBuildSettings;
     }
 
     void OnCollisionEnter(Collision other)
@@ -17,7 +19,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This is the starting point. Get going!");
                 break;
             case "Finish":
-                Debug.Log("Ship has reached destination. Stage complete.");
+                LoadNextLevel();
                 break;
             default: // reset level on ship crash
                 ReloadLevel();
@@ -27,6 +29,21 @@ public class CollisionHandler : MonoBehaviour
 
     void ReloadLevel()
     {
+        Debug.Log("Ship crashed, respawning at launch pad.");
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        if (currentSceneIndex + 1 < totalSceneCount)
+        {
+            Debug.Log("Destination reached, moving to next level.");
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            Debug.Log("Congratulations on finishing the game!");
+            SceneManager.LoadScene(0);
+        }
     }
 }
