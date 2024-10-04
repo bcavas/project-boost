d8fb24499f7c985a5527019a8343252567bf85d9
bcavas <35956598+bcavas@users.noreply.github.com>
2024-10-04 20:40:15 +0800

    play particle effects on rocket ship movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float forwardThrust = 800.0f;
     [SerializeField] float rotationalThrust = 20.0f;
     [SerializeField] AudioClip thrusterAudio;
+    [SerializeField] ParticleSystem forwardThrustParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
 
     // Start is called before the first frame update
     void Start()
@@ -42,14 +46,13 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            PlayParticles("main");
+            PlayThrusterAudio();
             rb.AddRelativeForce(Vector3.up * Time.deltaTime * forwardThrust);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(thrusterAudio);
-            }
         }
         else
         {
+            forwardThrustParticles.Stop();
             audioSource.Stop();
         }
     }
@@ -58,21 +61,64 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            PlayParticles("left");
             ApplyRotation(rotationalThrust);
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            PlayParticles("right");
             ApplyRotation(-rotationalThrust);
+        }
+
+        else
+        {
+            rightThrustParticles.Stop();
+            leftThrustParticles.Stop();
         }
     }
 
-    private void ApplyRotation(float rotationMagnitude)
+    void ApplyRotation(float rotationMagnitude)
     {
         // freeze physics system rotation to allow manual rotation
         // this resolves bug that removes player control upon collisions
         rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * Time.deltaTime * rotationMagnitude);
         rb.freezeRotation = false;
+    }
+
+    void PlayThrusterAudio()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrusterAudio);
+        }
+    }
+
+    void PlayParticles(String thruster)
+    {
+        if (thruster == "main")
+        {
+            if (!forwardThrustParticles.isPlaying)
+            {
+                forwardThrustParticles.Play();
+            }
+        }
+
+        if (thruster == "left")
+        {
+            if (!leftThrustParticles.isPlaying)
+            {
+                leftThrustParticles.Play();
+            }
+        }
+
+        if (thruster == "right")
+        {
+            if (!rightThrustParticles.isPlaying)
+            {
+                rightThrustParticles.Play();
+            }
+        }
     }
 }
